ccb33511b72dd46179c7a2a420b22942fab8dd69
Kahlil Wehmeyer <kahlilwehmeyer@gmail.com>
2017-12-05 16:22:01 -0500

    WIP

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@ using namespace std;
 void videoExtractor();
 void customerExtractor();
 void displayMenu();
+void addAVideo();
+void addACustomer();
 
 struct video {
     string title;
@@ -38,10 +40,9 @@ int i = 0;
 int j = 0;
 int main() { //starts the main function
 
-    customerExtractor();
-    videoExtractor();
+    //customerExtractor();
+    //videoExtractor();
     displayMenu();
-    cout << videos[0].stars << endl;
 
 
     return(0);
@@ -52,14 +53,14 @@ void customerExtractor() {
     ifstream infile;
     infile.open("/home/kahlil/CLionProjects/untitled1/customers.txt");
     do{
-        infile >> customers[i].firstName;
-        infile >> customers[i].lastName;
+        getline(cin, customers[i].firstName);
+        getline(cin, customers[i].lastName);
         infile >> customers[i].accountNumber;
-        infile >> customers[i].rental[0].title;
-        infile >> customers[i].rental[1].title;
-        infile >> customers[i].rental[2].title;
-        infile >> customers[i].rental[3].title;
-        infile >> customers[i].rental[4].title;
+        getline(cin, customers[i].rental[0].title);
+        getline(cin, customers[i].rental[1].title);
+        getline(cin, customers[i].rental[2].title);
+        getline(cin, customers[i].rental[3].title);
+        getline(cin, customers[i].rental[4].title);
         i++;
     } while (infile.good());
 }
@@ -68,28 +69,15 @@ void videoExtractor() {
     ifstream infile;
     infile.open("/home/kahlil/CLionProjects/untitled1/videos.txt");
     do{
-        infile >> videos[j].title;
-        infile >> videos[j].stars;
-        infile >> videos[j].producer;
-        infile >> videos[j].director;
-        infile >> videos[j].company;
+        getline(cin, videos[j].title);
+        getline(cin, videos[j].stars);
+        getline(cin, videos[j].producer);
+        getline(cin, videos[j].director);
+        getline(cin, videos[j].company);
         infile >> videos[j].copies;
         j++;
     } while (infile.good());
 }
-/*
-void customer::printInfo(){
-    cout << "First Name:" <<firstName << endl;
-    cout << "Last Name:" << lastName << end;
-    cout << "Acc. Number" << accountNumber << endl;
-    cout << "Rentals:" << endl;
-    cout << rental[0] << endl;
-    cout << rental[1] << endl;
-    cout << rental[2] << endl;
-    cout << rental[3] << endl;
-    cout << rental[5] << endl;
-}*/
-
 void printVideoList(){
     cout << ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::" << endl;
     cout << "VIDEOS ON FILE" << endl;
@@ -105,6 +93,50 @@ void printVideoList(){
 }cout << ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::" << endl;
     cout << "" << endl;
     cout << "" << endl;}
+/*void addACustomer() {
+    int openPos;
+    for(int q = 0; q <= 100; q++){
+        if(customers[q].firstName==""){
+            openPos = q;
+            cout << "Enter Customer First Name;" << endl;
+            cin >> customers[openPos].firstName;
+            cout << "Enter Customer Last Name;" << endl;
+            cin >> customers[openPos].lastName;
+            customers[openPos].accountNumber=openPos;
+            customers[openPos].rental[0] = "-";
+            customers[openPos].rental[0] = "-";
+            customers[openPos].rental[0] = "-";
+            customers[openPos].rental[3] = "-";
+            customers[openPos].rental[4] = "-";
+            break;
+        }
+    }
+    cout << customers[openPos].firstName << " has been added." << endl;
+}*/
+
+
+void addAVideo() {
+    int openPos;
+    for(int q = 0; q < 1000; q++){
+        if(videos[q].title==""){
+            openPos=q;
+            cout << " Enter Video title: " << endl;
+            getline(cin, videos[openPos].title);
+            cout << "Enter Stars: " << endl;
+            getline(cin, videos[openPos].stars);
+            cout << "Enter Director: " << endl;
+            getline(cin, videos[openPos].director);
+            cout << "Enter Producer: " << endl;
+            getline(cin, videos[openPos].producer);
+            cout << "Enter Company :" << endl;
+            getline(cin, videos[openPos].company);
+            cout << "Enter # of Copies: " << endl;
+            cin >> videos[openPos].copies;
+            break;
+        }
+    }
+    cout << videos[openPos].title << " has been added" << endl;
+}
 
 
 void displayMenu(){
@@ -125,10 +157,10 @@ void displayMenu(){
         cin >> selection;
 
         if(selection==1){
-
+            void addACustomer();
         }
         if(selection==2){
-
+            void addAVideo();
         }
         if(selection==3){
 
